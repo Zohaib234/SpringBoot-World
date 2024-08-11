79dda685698bfe3150c7822ee32e1a3000da5486
Zohaib Khoso <101723080+Zohaib234@users.noreply.github.com>
2024-08-11 13:27:48 +0500

    Delete CompanyMicroservice directory

--- CompanyMicroservice/src/main/java/com/zohaib/microservices/Company/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zohaib.microservices.Company;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company , Long> {
-
-}
